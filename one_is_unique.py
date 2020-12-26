d9cf6c042be86766c566f89bc90d707d15d89fe5
JamZYu <yzdsoy@umich.edu>
2020-12-26 13:50:56 -0800

    Prove for one is unique

---
+++ one_is_unique.py
@@ -0,0 +1,32 @@
+from z3 import *
+
+s = Solver()
+
+one = Int('one')
+zero = Int('zero')
+# constant c as another multiplication identity
+c = Int('c')
+
+# addition
+f = Function('f', IntSort(), IntSort(), IntSort())
+# multiplication
+g = Function('g', IntSort(), IntSort(), IntSort())
+# addition inverse
+n = Function('n', IntSort(), IntSort())
+
+ground_term = [one, zero, c]
+
+for x in ground_term:
+    s.add(And(
+        g(x, one) == x,
+        g(one, x) == x
+        ))
+
+for x in ground_term:
+    s.add(And(
+        g(x, c) == x,
+        g(c, x) == x,
+        c != one
+    ))
+
+print(s.check())
